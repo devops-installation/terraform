0012ea4a93668832d49d6cde98edaaf27edbf8ca
pratik <pratik@GMAIL.COM>
2024-09-05 10:01:47 +0530

    latest code

---
+++ main.tf
@@ -0,0 +1,14 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred region
+}
+
+# Create an EC2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c71c99"  # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"               # Change as per your requirements
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
