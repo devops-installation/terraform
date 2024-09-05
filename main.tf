126adc79dea092beeebd3e46b37c68c85c3b7b7f
pratik <pratik@GMAIL.COM>
2024-09-05 12:22:41 +0530

    latest code

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"  # Change to your preferred region
+  region = "us-west-2"
 }
 
-# Create an EC2 instance
-resource "aws_instance" "example" {
-  ami           = "ami-0c94855ba95c71c99"  # Replace with a valid AMI ID for your region
-  instance_type = "t2.micro"               # Change as per your requirements
+# # Create an EC2 instance
+# resource "aws_instance" "example" {
+#   ami           = "ami-0c94855ba95c71c99"  # Replace with a valid AMI ID for your region
+#   instance_type = "t2.micro"               # Change as per your requirements
 
-  tags = {
-    Name = "MyEC2Instance"
-  }
-}
+#   tags = {
+#     Name = "MyEC2Instance"
+#   }
+# }
