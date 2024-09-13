9e52c6238625f00efe98d2b9b7e39a8e9182c741
Shubham Phanse <phanases@gmail.com>
2024-09-13 22:35:15 +0530

    latest code

--- vpc_subnate/vpc_subnate.tf
+++ vpc_subnate/vpc_subnate.tf
@@ -6,30 +6,35 @@ variable "vpc_cidr_block1" {}
 variable "sub_cidr_block1" {}
 variable "env_prefix" {}
 variable "az" {}
-#vpc
+# VPC Resource
 resource "aws_vpc" "RH-vpc" {
-    cidr_block = var.vpc_cidr_block1
-    tags = {
-      Name = "${var.env_prefix}-vpc"
-    }
+  cidr_block = var.vpc_cidr_block1
+
+  tags = {
+    Name = "${var.env_prefix}-vpc"
+  }
 }
-#subnate
+
+# Subnet Resource
 resource "aws_subnet" "RH_subnate-1" {
-    vpc_id = aws_vpc.RH-vpc.id
-    cidr_block = var.sub_cidr_block1
-    availability_zone = var.az
-    tags = {
-      Name = "${var.env_prefix}-subnate-1"
-    }
+  vpc_id            = aws_vpc.RH-vpc.id
+  cidr_block        = var.sub_cidr_block1
+  availability_zone = var.az
+
+  tags = {
+    Name = "${var.env_prefix}-subnate-1"
+  }
 }
-#igw
+
+# Internet Gateway Resource
 resource "aws_internet_gateway" "RH_igw" {
   vpc_id = aws_vpc.RH-vpc.id
+
   tags = {
     Name = "${var.env_prefix}-igw"
   }
-  
 }
+
 # # route table
 # resource "aws_route_table" "RH_rout_table" {
 #   vpc_id = aws_vpc.RH-vpc.id
@@ -47,16 +52,22 @@ resource "aws_internet_gateway" "RH_igw" {
 #   route_table_id = aws_route_table.RH_rout_table.id
 # }
 
-# default route table
+# Default Route Table: Modify default route table for the VPC
 resource "aws_default_route_table" "RH_default_route_table" {
-  default_route_table_id = "rtb-041ef4121b31bd99f"
+  default_route_table_id = aws_vpc.RH-vpc.default_route_table_id  # Use the default route table of the VPC
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.RH_igw.id
   }
+
+  tags = {
+    Name = "${var.env_prefix}-default-route-table"
+  }
 }
-# rout table association
+
+# Route Table Association
 resource "aws_route_table_association" "RH_a_rta_subnate" {
-  subnet_id = aws_subnet.RH_subnate-1.id
+  subnet_id      = aws_subnet.RH_subnate-1.id
   route_table_id = aws_default_route_table.RH_default_route_table.id
 }
