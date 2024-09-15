5accc53b3788ab6235b142a492ca85ec940229a5
Shubham Phanse <phanases@gmail.com>
2024-09-15 16:27:21 +0530

    latest code

--- vpc_subnate/vpc_subnate.tf
+++ vpc_subnate/vpc_subnate.tf
@@ -106,7 +106,7 @@ data "aws_ami" "RH-FE-ubuntu" {
   most_recent = true
   owners = ["099720109477"]
   filter {
-    name = name
+    name = "name"
     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
   filter {
