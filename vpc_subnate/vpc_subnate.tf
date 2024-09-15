4acb970a6153cbcb5d8071fa30a7feb71bc4adf8
Shubham Phanse <phanases@gmail.com>
2024-09-15 23:33:21 +0530

    latest code

--- vpc_subnate/vpc_subnate.tf
+++ vpc_subnate/vpc_subnate.tf
@@ -137,13 +137,7 @@ resource "aws_instance" "RH-FE" {
   vpc_security_group_ids = [aws_security_group.RH_sg.id]
   associate_public_ip_address = true
 
-  user_data = <<EOF
-                  #!/bin/bash
-                  sudo apt update
-                  sudo apt install -y nginx
-                  sudo systemctl start nginx
-                  sudo systemctl enable nginx
-              EOF
+  user_data = file("entry-script.sh")
 
   tags = {
     Name = "${var.env_prefix}-RH-FE-web"
