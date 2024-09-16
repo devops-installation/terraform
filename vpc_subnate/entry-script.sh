db185cc135bda648803d37ae758313344daeff9b
Shubham Phanse <phanases@gmail.com>
2024-09-16 11:11:27 +0530

    latest code

--- vpc_subnate/entry-script.sh
+++ vpc_subnate/entry-script.sh
@@ -1,30 +1,5 @@
-# #!/bin/bash
-# sudo apt update
-# sudo apt install -y nginx
-# sudo systemctl start nginx
-# sudo systemctl enable nginx
 #!/bin/bash
-
-# Update the package index
 sudo apt update
-
-# Install Docker dependencies
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-
-# Add the Docker repository
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable'
-
-# Update the package index again
-sudo apt update
-
-# Install Docker
-sudo apt install -y docker-ce
-
-# Start Docker
-sudo systemctl start docker
-
-# Enable Docker to start at boot
-sudo systemctl enable docker
-sudo docker run -d --name nginx -p 80:80 nginx
+sudo apt install -y nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
