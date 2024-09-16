2e2a56914e2c10aedad6a21679c0c2a2f31f19c3
Shubham Phanse <phanases@gmail.com>
2024-09-16 16:51:36 +0530

    latest code

--- vpc_subnate/vpc_subnate.tf
+++ vpc_subnate/vpc_subnate.tf
@@ -129,7 +129,7 @@ output "aws_ami_id" {
   value = data.aws_ami.RH-FE-ubuntu.id
 }
 output "public_ip" {
-  value = aws_instance.RH-FE.id
+  value = aws_instance.RH-FE.public_ip
 }
 # key pair
 resource "aws_key_pair" "ubuntu_key" {
