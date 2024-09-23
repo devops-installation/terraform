c53b8490be6ffaedcfc564df2a86e9c6385fc497
Shubham Phanse <phanases@gmail.com>
2024-09-23 22:02:04 +0530

    latest code

--- aws/output.tf
+++ aws/output.tf
@@ -1,4 +1,13 @@
-output "ec2" {
-    value = aws_instance.ec2
+output "ec2_details" {
+    value = [
+        aws_instance.ec2.public_ip,
+        aws_instance.ec2.private_ip,
+        aws_instance.ec2.id
+        aws_instance.ec2.availability_zone,
+        aws_instance.ec2.ami,
+        aws_instance.ec2.key_name,
+       aws_instance.ec2.memory_size,
+        aws_instance.ec2.root_block_device[0].volume_size
+    ]
   
 }
