e3f9b038cf9f7a19e33c55d8558566a710afc1c3
Shubham Phanse <phanases@gmail.com>
2024-09-23 22:11:37 +0530

    latest code

--- aws/output.tf
+++ aws/output.tf
@@ -1,13 +1,11 @@
 output "ec2_details" {
-    value = [
-        aws_instance.ec2.public_ip,
-        aws_instance.ec2.private_ip,
-        aws_instance.ec2.id,
-        aws_instance.ec2.availability_zone,
-        aws_instance.ec2.ami,
-        aws_instance.ec2.key_name,
-        # aws_instance.ec2.memory_size,
-        aws_instance.ec2.root_block_device[0].volume_size
-    ]
-  
+    value = {
+        public_ip    = aws_instance.ec2.public_ip
+        private_ip   = aws_instance.ec2.private_ip
+        id           = aws_instance.ec2.id
+        az           = aws_instance.ec2.availability_zone
+        ami          = aws_instance.ec2.ami
+        key          = aws_instance.ec2.key_name
+        volume_size  = aws_instance.ec2.root_block_device[0].volume_size
+    }
 }
