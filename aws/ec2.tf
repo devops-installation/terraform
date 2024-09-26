636bad46d9d5f1d3c6f3c4e1921366cf1c178767
Shubham Phanse <phanases@gmail.com>
2024-09-26 20:09:31 +0530

    latest code

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -44,7 +44,25 @@ resource "aws_security_group" "nginx-sg" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
   
 }
 resource "aws_ebs_volume" "ec2_vol" {
