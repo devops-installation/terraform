87ee93d9518ee87f984cb37263e453b5e5c226f8
Shubham Phanse <phanases@gmail.com>
2024-09-26 22:37:27 +0530

    latest code

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -8,17 +8,17 @@ data "aws_ami" "os" {
     }
   
 }
-# # this conf provide you mannual key 
-# resource "aws_key_pair" "org-key" {
-#     key_name   = "org-key"
-#     public_key = 
-# }
+# this conf provide you mannual key 
+resource "aws_key_pair" "ngx-key" {
+    key_name   = "ngx-key"
+    public_key = file("/home/shubham/.ssh/id_rsa.pub")
+}
 
 
 resource "aws_instance" "ec2" {
     ami = data.aws_ami.os.id
     instance_type = "t2.micro"
-    key_name = "org-key"
+    key_name = aws_key_pair.ngx-key.key_name
     vpc_security_group_ids = [aws_security_group.nginx-sg.id]
     subnet_id = aws_subnet.pub-sub.id
     associate_public_ip_address = true
@@ -32,7 +32,24 @@ resource "aws_instance" "ec2" {
     #             sudo systemctl enable nginx
 	#      EOF
 
-    user_data = file("entry-script.sh")
+    # user_data = file("entry-script.sh")
+
+    connection {
+      type = "ssh"
+      host = aws_instance.ec2.public_ip
+      user = "ubuntu"
+      private_key = "/home/shubham/.ssh/id_rsa"
+    }
+
+    provisioner "remote-exec" {
+        inline = [ 
+            "sudo apt install -y nginx",
+            "sudo systemctl start nginx",
+            "sudo systemctl enable nginx",
+            "mkdir shubham"
+         ]
+      
+    }
 
    tags = {
     Name = "nginx-web-server"
