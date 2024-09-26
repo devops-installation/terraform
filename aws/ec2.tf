33f27f087b57654f369503b16e1b83f28c446d69
Shubham Phanse <phanases@gmail.com>
2024-09-26 20:01:04 +0530

    latest code

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -6,6 +6,13 @@ resource "aws_vpc" "my-vpc" {
         Name = "my-vpc"
     }  
 }
+resource "aws_internet_gateway" "ngx-igw" {
+    vpc_id = aws_vpc.my-vpc.id
+    tags = {
+      Name = "ngx-igw"
+    }
+  
+}
 resource "aws_subnet" "pub-sub" {
     cidr_block = "10.0.1.0/24"
     vpc_id = aws_vpc.my-vpc.id
