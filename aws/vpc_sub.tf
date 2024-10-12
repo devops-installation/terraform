6cd2e12796102a4be167a4f938a016390fee0b8a
Shubham Phanse <phanases@gmail.com>
2024-10-12 18:17:04 +0530

    latest code

--- aws/vpc_sub.tf
+++ aws/vpc_sub.tf
@@ -17,3 +17,13 @@ resource "aws_subnet" "pub-sub" {
     availability_zone = "us-west-2a"
   
 }
+resource "aws_default_route_table" "default_rout_table" {
+    default_route_table_id = aws_vpc.my-vpc.default_route_table_id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.ngx-igw.id
+    }
+    tags = {
+      name  = "ngx-rout-table"
+    } 
+}
