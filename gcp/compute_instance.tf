b6df27944c4aa94285d648a941706eab134586c8
Shubham Phanse <phanases@gmail.com>
2024-10-14 15:59:37 +0530

    latest code

--- gcp/compute_instance.tf
+++ gcp/compute_instance.tf
@@ -1,8 +1,8 @@
-resource "google_service_account" "default" {
-     # The unique ID for the service account
-  account_id = "2520f7d965511976b9e442b2560731371fa79cb2" 
-  display_name = "SA-vm"
-}
+# resource "google_service_account" "default" {
+#      # The unique ID for the service account
+#   account_id = "2520f7d965511976b9e442b2560731371fa79cb2" 
+#   display_name = "SA-vm"
+# }
 resource "google_compute_instance" "RH-staging" {
     name = "Rh-staging"
     machine_type = "e2-medium"
@@ -11,19 +11,19 @@ resource "google_compute_instance" "RH-staging" {
     boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"  # Choose the boot image
+      }
     }
-  }
     scratch_disk {
     interface = "NVME"
-  }
+     }
      network_interface {
-    network       = "default"   # Replace with your VPC network
-    access_config {}            # Allows external (public) IP access
-  }
-    service_account {
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]  # Define the scopes the VM has access to
-  }
+        network       = "default"   # Replace with your VPC network
+        access_config {}            # Allows external (public) IP access
+    }
+#     service_account {
+#     email  = google_service_account.default.email
+#     scopes = ["cloud-platform"]  # Define the scopes the VM has access to
+#   }
 }
 
 
