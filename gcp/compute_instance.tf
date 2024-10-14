5a5d660a123dd7968cf0d4514f7576dac254ddcd
Shubham Phanse <phanases@gmail.com>
2024-10-14 16:32:51 +0530

    latest code

--- gcp/compute_instance.tf
+++ gcp/compute_instance.tf
@@ -11,6 +11,7 @@ resource "google_compute_instance" "rh-staging" {
     boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"  # Choose the boot image
+      size = 23
       }
     }
     # scratch_disk {
@@ -20,11 +21,17 @@ resource "google_compute_instance" "rh-staging" {
         network       = "default"   # Replace with your VPC network
         access_config {}            # Allows external (public) IP access
     }
+     metadata_startup_script = <<-EOT
+        #!/bin/bash
+        sudo apt-get update
+        sudo apt-get install -y apache2
+    EOT
+}
 #     service_account {
 #     email  = google_service_account.default.email
 #     scopes = ["cloud-platform"]  # Define the scopes the VM has access to
 #   }
-}
+
 
 
 
