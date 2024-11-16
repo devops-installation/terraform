29ad8379ceca93d0f2f63e7bdad35e78ff850272
Shubham Phanse <phanases@gmail.com>
2024-11-16 18:31:49 +0530

    latest code

--- gcp/compute_instance.tf
+++ gcp/compute_instance.tf
@@ -13,7 +13,7 @@ resource "google_compute_instance" "rh-staging-master" {
     boot_disk {
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"  # boot image ubuntu 24.04 lts
-      size = 20
+      size = 30
       }
     }
     # scratch_disk {
@@ -57,7 +57,55 @@ resource "google_compute_instance" "rh-staging-master" {
 #     scopes = ["cloud-platform"]  # Define the scopes the VM has access to
 #   }
 
-
+resource "google_compute_instance" "rh-staging-node" {
+    name = "rh-stage-node"
+    machine_type = "e2-medium"
+    zone = "us-central1-a"
+    tags = ["foo", "bar"] #network tag
+    # hostname     = "rh-staging-master"
+    
+    boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"  # boot image ubuntu 24.04 lts
+      size = 30
+      }
+    }
+    # scratch_disk {
+    # interface = "NVME"
+    #  }
+     network_interface {
+        network       = "default"   # Replace with your VPC network
+        access_config {}            # Allows external (public) IP access
+    }
+   #  metadata_startup_script = file("entry-script.sh")
+     
+     metadata = {
+        ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+     }
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"                     # Use the 'ubuntu' user for connection
+      private_key = file("~/.ssh/id_rsa")        # Path to your private key
+      host        = google_compute_instance.rh-staging-master.network_interface[0].access_config[0].nat_ip  # Get the VM public IP
+    }
+     provisioner "remote-exec" {
+    
+    # Use the entry-script.sh file for inline execution
+    #script = "/home/shubham/terraform/gcp/entry-script.sh"  # Read the script from the current module path
+    # #OR
+      inline = [
+      "sudo apt-get update",
+      "git clone https://github.com/devops-installation/kubernetes.git",
+      "chmod -775 kubernetes/cluster-setup/k8.sh",
+      "sh kubernetes/cluster-setup/k8.sh"
+    ]
+  }
+    #  metadata_startup_script = <<-EOT
+    #     #!/bin/bash
+    #     sudo apt-get update
+    #     sudo apt-get install -y apache2
+    #  EOT
+}
 
 
 
